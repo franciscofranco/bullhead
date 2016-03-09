b49407d81eddbaaf2560eea9e0aa09bb1a31b12d
Roopesh Rajashekharaiah Nataraja <roopeshr@codeaurora.org>
2016-03-09 15:40:25 -0800

    qseecom: don't release ion share memory if scm_call unload TA failed
    If a scm_call request to shutdown a TA fails, the TA is not shut down
    and still in use, and the resources aren't necessarily leaked. Since
    shared memory are still locked in this situation, ion shared memory
    cannot be released, otherwise XPU violation occurs. Only need to
    release shared memory if TA is unloaded success or that TA cannot
    be found.
    
    Change-Id: I971485fb541193f77960cc7ca14b5b09de938a43
    
    Bug: 31220655
    Signed-off-by: Roopesh Rajashekharaiah Nataraja <roopeshr@codeaurora.org>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -1584,7 +1584,7 @@ static int qseecom_unload_app(struct qseecom_dev_handle *data,
 			pr_err("scm_call to unload app (id = %d) failed\n",
 								req.app_id);
 			ret = -EFAULT;
-			goto unload_exit;
+			goto not_release_exit;
 		} else {
 			pr_warn("App id %d now unloaded\n", req.app_id);
 		}
@@ -1592,7 +1592,7 @@ static int qseecom_unload_app(struct qseecom_dev_handle *data,
 			pr_err("app (%d) unload_failed!!\n",
 					data->client.app_id);
 			ret = -EFAULT;
-			goto unload_exit;
+			goto not_release_exit;
 		}
 		if (resp.result == QSEOS_RESULT_SUCCESS)
 			pr_debug("App (%d) is unloaded!!\n",
@@ -1603,7 +1603,7 @@ static int qseecom_unload_app(struct qseecom_dev_handle *data,
 			if (ret) {
 				pr_err("process_incomplete_cmd fail err: %d\n",
 									ret);
-				goto unload_exit;
+				goto not_release_exit;
 			}
 		}
 	}
@@ -1633,6 +1633,7 @@ static int qseecom_unload_app(struct qseecom_dev_handle *data,
 unload_exit:
 	qseecom_unmap_ion_allocated_memory(data);
 	data->released = true;
+not_release_exit:
 	return ret;
 }
 
